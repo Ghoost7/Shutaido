437d6346dcdceb5edf5a9ed8a77bfab7cc7ef3b0
Ghoost7 <gu.hervy@gmail.com>
2023-07-27 14:59:39 +0200

    modif apostrophe

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,35 +13,33 @@ const Hero = ({heading, message}) => {
             
                 <div>
             <h1 className='text-center mb-4'>A Propos</h1>
-            <p className='text-center'>PHILIPPE APRUZZESE, penseur inspiré, vous invite à entrer dans le monde du SHUTAÏDO©️, une méthode d'approche du bien-être humain, qu'il a conçue, mis au point et qu'il perfectionne jour après jour.
+            <p className='text-center'>PHILIPPE APRUZZESE, penseur inspiré, vous invite à entrer dans le monde du SHUTAÏDO©️, une méthode d&aposapproche du bien-être humain, qu&aposil a conçue, mis au point et qu&aposil perfectionne jour après jour.
                     
-                    ​Le SHUTAÏDO©️ est né en 1985 en Italie. En 1990, je décide de déposer le symbole et le nom à Rome. Pendant vingt années de réflexions, d’exercices pratiques et de différentes sources d’inspirations, j’élabore cette nouvelle méthode d’approche du bien-être humain.  Méthode originale que je perfectionne de jour en jour.
-                    S’en suivent deux autres dépôts (2010 et 2015, sous le régime de la Convention de Berne) droits d’auteur, droits à la propriété intellectuelle. 
+                    ​Le SHUTAÏDO©️ est né en 1985 en Italie. En 1990, je décide de déposer le symbole et le nom à Rome. Pendant vingt années de réflexions, d&aposexercices pratiques et de différentes sources d&aposinspirations, j&aposélabore cette nouvelle méthode d&aposapproche du bien-être humain.  Méthode originale que je perfectionne de jour en jour.
+                    S&aposen suivent deux autres dépôts (2010 et 2015, sous le régime de la Convention de Berne) droits d&aposauteur, droits à la propriété intellectuelle. 
                     En voici le principe déclaré : <br />
-                    "Le SHUTAÏDO©️ est une discipline spirituelle, cosmogonique, procédant d’une gestuelle programmée selon une méthodologie précise, et assujettie à la volonté du sujet, qui a pour objectif central la production d’un bien-être personnel". 
+                    &quotLe SHUTAÏDO©️ est une discipline spirituelle, cosmogonique, procédant d&aposune gestuelle programmée selon une méthodologie précise, et assujettie à la volonté du sujet, qui a pour objectif central la production d&aposun bien-être personnel&quot. 
                     En voici les principes énergétiques et spirituels : <br />
-                    Le SHUTAÏDO©️ est une discipline où chacun explore librement toute une série de mouvements. C’est une pratique énergétique Universelle. Les énergies Universelles circulent librement (libre : qui n’est pas la propriété d’un maître, d’un courant philosophique, d’un dogme, d'une doctrine).
-                    De fait l’énergie est libre et universelle ! dénuée (« dénudare » mettre à nu) de toutes doctrines, de tous concepts, de tous systèmes ! <br />
+                    Le SHUTAÏDO©️ est une discipline où chacun explore librement toute une série de mouvements. C&aposest une pratique énergétique Universelle. Les énergies Universelles circulent librement (libre : qui n&aposest pas la propriété d&aposun maître, d&aposun courant philosophique, d&aposun dogme, d&aposune doctrine).
+                    De fait l&aposénergie est libre et universelle ! dénuée (« dénudare » mettre à nu) de toutes doctrines, de tous concepts, de tous systèmes ! <br />
 
-                    Je vous propose de retrouver votre autonomie, votre savoir-faire, aux travers de diverses ateliers d’initiations pédagogiques. <br />
-                    Le Shutaïdo est une Gymnastique originale qui permet dès les premiers mouvements de se mettre en phase magnétique avec la Terre et... l’univers. De retrouver son Identité intérieure, sa propre dimension spirituelle ce qui apporte autonomie et... "liberté d’Être". <br />
+                    Je vous propose de retrouver votre autonomie, votre savoir-faire, aux travers de diverses ateliers d&aposinitiations pédagogiques. <br />
+                    Le Shutaïdo est une Gymnastique originale qui permet dès les premiers mouvements de se mettre en phase magnétique avec la Terre et... l&aposunivers. De retrouver son Identité intérieure, sa propre dimension spirituelle ce qui apporte autonomie et... &quotliberté d&aposÊtre&quot. <br />​
 
-                    ​
-
-                    C’est un Art qui a la particularité d’être en évolution permanente  où chacun expérimente librement les courants d’énergies qui l’animent aux travers de divers mouvements spécifiques. Les changements de paramètres actuels bouleversent notre biorythme, notre boussole intérieure est déréglée. L’homme, s'il n’est plus en phase magnétique, "peut perdre le nord". 
+                    C&aposest un Art qui a la particularité d&aposêtre en évolution permanente  où chacun expérimente librement les courants d&aposénergies qui l&aposaniment aux travers de divers mouvements spécifiques. Les changements de paramètres actuels bouleversent notre biorythme, notre boussole intérieure est déréglée. L&aposhomme, s&aposil n&aposest plus en phase magnétique, &quotpeut perdre le nord&quot. 
 
                     ​<br />
 
-                    Le premier mouvement, à lui seul, permet de se mettre en phase magnétique avec la Terre chaque jour, de s'ancrer, de s'aligner, de se centrer (cela prend trente secondes!). Les techniques sont simples, bénéfiques et accessibles à tous. Ils peuvent aussi être pratiqués assis. <br />
+                    Le premier mouvement, à lui seul, permet de se mettre en phase magnétique avec la Terre chaque jour, de s&aposancrer, de s&aposaligner, de se centrer (cela prend trente secondes!). Les techniques sont simples, bénéfiques et accessibles à tous. Ils peuvent aussi être pratiqués assis. <br />
 
-                    « Le shutaido est une discipline spirituelle, cosmogonique, procédant d’une série de mouvements  programmés selon une méthodologie précise, et qui s’adapte instantanément aux nouveaux courants perpétuels universels.  
+                    « Le shutaido est une discipline spirituelle, cosmogonique, procédant d&aposune série de mouvements  programmés selon une méthodologie précise, et qui s&aposadapte instantanément aux nouveaux courants perpétuels universels.  
                     <br />
-                    L’efficacité de cette méthode repose sur l’incorporation de nouveaux courants d’énergies que le mouvement engendre. »
+                    L&aposefficacité de cette méthode repose sur l&aposincorporation de nouveaux courants d&aposénergies que le mouvement engendre. »
                     Et en toutes circonstances,  garder votre libre arbitre! 
                     <br />
-                   <br /> "Ce sont ceux qui pensent par eux-mêmes et qui ne sont pas régis par des esprits de groupe ou des égrégores de groupe organisés, qui parviendront à l’Unité, véritable but de toute l’évolution de la conscience" 
+                   <br /> &quotCe sont ceux qui pensent par eux-mêmes et qui ne sont pas régis par des esprits de groupe ou des égrégores de groupe organisés, qui parviendront à l&aposUnité, véritable but de toute l&aposévolution de la conscience&quot 
 
-                    Auteur : JACQUELINE BOUSQUET (1927-2013) Docteur ès-Sciences, Biologiste, Chercheur honoraire au CNRS - livre :«  AU COEUR DU VIVANT" - éditions Ecopolia . Collection Science en Conscience.
+                    Auteur : JACQUELINE BOUSQUET (1927-2013) Docteur ès-Sciences, Biologiste, Chercheur honoraire au CNRS - livre :«  AU COEUR DU VIVANT - éditions Ecopolia . Collection Science en Conscience.
 
                 </p>   
         
